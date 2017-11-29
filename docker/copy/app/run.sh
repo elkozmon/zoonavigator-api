5cde3ee9e9597ae5c0af698a9cf163e68ac28e79
Lubos Kozmon <lubosh91@gmail.com>
2017-11-30 00:05:27 +0100

    Add Docker JVM config

--- docker/copy/app/run.sh
+++ docker/copy/app/run.sh
@@ -6,8 +6,14 @@ rm -f "/app/RUNNING_PID"
 # Generate random crypto secret if not defined
 export CRYPTO_SECRET=${CRYPTO_SECRET:-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | fold -w 64 | head -n 1)}
 
+# JVM config
+JVM_CONFIG="-J-server"
+
+[ -n "$JVM_XMS" ] && JVM_CONFIG="$JVM_CONFIG -J-Xms$JVM_XMS"
+[ -n "$JVM_XMX" ] && JVM_CONFIG="$JVM_CONFIG -J-Xmx$JVM_XMX"
+
 # Start application
-/app/bin/zoonavigator-play &
+/app/bin/zoonavigator-play ${JVM_CONFIG} &
 APP_PID=$!
 
 # Trap terminate signals
