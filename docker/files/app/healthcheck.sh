34c0bdc02961b9cb4f0b8f21d885ef505bed2f3f
Lubos Kozmon <lubosh91@gmail.com>
2018-05-21 21:06:20 +0200

    Fix healthcheck script

--- docker/files/app/healthcheck.sh
+++ docker/files/app/healthcheck.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-curl -f http://localhost:${API_HTTP_PORT}/api/healthcheck || exit 1
+# TODO Remove 'SERVER_HTTP_PORT' in 1.0.0
+curl -f http://localhost:${SERVER_HTTP_PORT:-$API_HTTP_PORT}/api/healthcheck || exit 1
